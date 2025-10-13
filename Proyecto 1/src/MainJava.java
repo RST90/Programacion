f3f413e1fb17ac54982e55ca3eb3e08770649f24
RST90 <rafa.st_@hotmail.com>
2025-10-13 20:55:31 +0200

    Update MainJava.java

--- Proyecto 1/src/MainJava.java
+++ Proyecto 1/src/MainJava.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
 public class MainJava {
     public static void main(String[] args){
-System.out.println("Hola, Rafa");
-System.out.println("Bienvenido a tu primer programa en Java");
+Scanner entrada= new Scanner(System.in);
+System.out.print("¿Cómo te llamas? ");
+String nombre = entrada.nextLine();
 
+System.out.print("¿Cuántos años tienes? ");
+int edad = entrada.nextInt();
+
+System.out.println("Hola, " + nombre + ". Tienes " + edad + " años");
+if (edad>18) {
+    System.out.println("Eres mayor de edad.");
+} else {
+    System.out.println("eres menor de edad.");
+}
     }
 
 }
