61b03f88a5c5eefe9fc4f0c78eb0eccff29f5675
RST90 <rafa.st_@hotmail.com>
2025-10-14 19:18:52 +0200

    Update
    Realizo el ejercicio 2

---
+++ Ejercicios/ejercicio2/src/ejercicio2.java
@@ -0,0 +1,11 @@
+public class ejercicio2 {
+    public static void main(String[] args) {
+        int puntuacion = 3;
+
+        puntuacion += 8;
+        puntuacion -= 5;
+        puntuacion /= 1;
+
+       System.out.println("La puntuacion final es = " + puntuacion);
+    }
+}
