873b23894e5a0e2e3036c1585a0b38e141ff5fc3
RST90 <rafa.st_@hotmail.com>
2025-10-15 11:17:26 +0200

    update
    Ejercicio 4 hecho

---
+++ Ejercicios/Ejercicio 4/src/ejercicio4.java
@@ -0,0 +1,19 @@
+package PACKAGE_NAME;
+
+public class ejercicio4 {
+    public static void main(String[] args) {
+        String titulolibro = "Los renglones torcidos de Dios";
+        String autorlibro = "Torcuato Luca de Tena Brunet";
+
+        int anioPublicacion = 1979;
+        int numerodepaginas =  512;
+
+        boolean estaDisponible = true;
+
+        System.out.println( " titulo libro = " + titulolibro);
+        System.out.println( " autor libro = " + autorlibro);
+        System.out.println( " fecha publicacion= " + anioPublicacion);
+        System.out.println( " número de páginas = " + numerodepaginas);
+        System.out.println( " está a la venta = " + estaDisponible);
+    }
+    }
