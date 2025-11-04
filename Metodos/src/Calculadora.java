8b2d9ff88b4fdfe9cf0b88fcea5712fb7ee1894a
RST90 <rafa.st_@hotmail.com>
2025-11-04 12:06:20 +0100

    update

--- Metodos/src/Calculadora.java
+++ Metodos/src/Calculadora.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 
 public class Calculadora {
     public void restar(int op1, int op2){
@@ -9,7 +10,46 @@ public class Calculadora {
         int resultado = op1+op2;
         System.out.println("El resultado de la suma es = " +resultado);
 
+        int operandoUno = 50;
+        int operandoDos = 10;
+        int resultados = operandoUno/operandoDos;
+        double division = operandoUno/operandoDos;
+        System.out.println("El resultado de la division es = " +resultados);
+
     }
+    public void multi (int op1, int op2) {
+        int resultado = op1 * op2;
+        System.out.println("El resultado de la multiplicacion es = " + resultado);
+
+    }
+    public void multip (){
+        int valorUno = 4;
+        int valorDos = 5;
+        int resultado = valorUno*valorDos;
+        System.out.println("El resultado de la nueva multiplicacion es = " +resultado);
+    }
+    public void operadoresLogicos (){
+        int sueldo = 20000;
+        int edad = 45;
+        boolean conducir = false;
+        boolean candidatoValido = sueldo<30000 && edad<40 && conducir;
+        System.out.println("El candidato es valido = " +candidatoValido);
+    }
+    public void operadorLogico (){
+        int sueldo = 25000;
+        int edad = 20;
+        boolean estudiosUniversitarios = true;
+        boolean candidatoAceptado = sueldo >26000 || edad <30 || estudiosUniversitarios;
+        System.out.println("El candidato es aceptable para el puesto = " +candidatoAceptado);
+    }
+
+    public void evaluarCandidato (double salario, int edad, boolean cochePropio){
+        boolean resultado = salario<20000 && edad<35 && cochePropio;
+        System.out.println("La evaluacion del candidato es = " +resultado);
+
+    }
+
+
 
 
     }
@@ -25,5 +65,29 @@ seria Calculadora()
 
 Luego simplemente ejecuto la variable creada operacionesMatematicas poniendolo un punto y la funcion que quiero que haga:
 operacionesMatematicas.sumar() y dentro del parentesis meto las variables que quiero dependiendo de si es int, double o float
+
+También se puede hacer desde esta clase por ejemplo con la division. he creado unas variables nuevas tanto a valores de operandoUno y operandoDos
+como a la funcion dividir y luego he puesto resultados en plural porque en singular ya lo tengo definido dentro de la misma clase.
+
+con la multiplicacion he querido comprobar si se podía hacer de las dos formas, declarando las variables en el metodo main y a su vez haciendolo en esta clase
+Para ello hay que generar dos variables distintas, no puede tener el mismo nombre, a una la llamo multi y la otra multip.
+En multi lo que hago es asignarle los valores en Main poniendo operacionesMatematicas.multi (valor1 y valor2), pero en multip se los asigno desde esta clase y
+por lo que al crear la nueva clase void no pongo nada entre parentesis como sí lo hago cuando le doy el valor en Main que sería:
+public void multi(op1*op2); aquí sería public void multip(){ y debajo es cuando pongo los valores de las variables y la operación y lo que quiero que me saque por consola
+Luego me voy a Main y añadiria operacionesMatematicas.multip();
+
+En operadoresLogicos estamos haciendo comparaciones a traves de si algo nos es valido o no, por ejemplo en este caso estamos usando el comparador && que sería un "and"
+y en este caso en cuanto una de las variables es false, el resultado va a ser false, para que el resultado fuese true tendrían que ser todas true.
+En el siguiente caso que uso otra funcion que es operadorLogico estoy usando el or que sería con || y en este caso con que haya un true siempre va a ser true,
+solo podría ser false en el caso de que todas las variables sean falsas.
+
+Otra manera sería con el Scanner, lo dejo detallado también en Main porque es donde se escriben las funciones. aquí voy a crear otra función que haré referencia en Main
+creo la funcion public void evaluarCandidato y le digo el tipo de variables que voy a usar (double para salario, int para edad y boolean para un true o false para el tema carnet)
+Como el resultado que obtendré será un true or false para evaluar si el candidato es valido o no voy a usar un boolean:
+boolean resultado = salario<20000 && edad<35 && cochePropio. Y luego pido el resultado por consola.
+Al usar && en cuanto uno de los valores sea false me va a dar false.
+
+
 */
+
 
--- Metodos/src/Main.java
+++ Metodos/src/Main.java
@@ -1,30 +1,55 @@
+import java.util.Scanner;
+
 public class Main{
 
 public static void main(String[] args) {
     Calculadora operacionesMatematicas = new Calculadora ();
     operacionesMatematicas.sumar(2, 8);
     operacionesMatematicas.restar(8, 89);
+    operacionesMatematicas.multi(3, 8);
+    operacionesMatematicas.multip();
+    operacionesMatematicas.operadoresLogicos();
+    operacionesMatematicas.operadorLogico();
+    Scanner evaluarCandidato = new Scanner(System.in);
+    System.out.println("¿Qué salario quieres ganar?");
+    double salario = evaluarCandidato.nextDouble();
+    System.out.println("¿Qué edad tienes?");
+    int edad = evaluarCandidato.nextInt();
+    System.out.println("¿Tienes carnet y coche propio?");
+    boolean cochePropio = evaluarCandidato.hasNextBoolean();
+    operacionesMatematicas.evaluarCandidato(salario, edad,cochePropio);
 
 
-    System.out.println("Hola, vamos a practicar");
-    System.out.println("Pues vamos a empezar por saludar");
-    saludar("Rafa");
-    saludar("Kyra");
-    saludar("Sacha");
+   // System.out.println("Hola, vamos a practicar");
+    //System.out.println("Pues vamos a empezar por saludar");
+    //saludar("Rafa");
+    //saludar("Kyra");
+    //saludar("Sacha");
 }
 
-public static void saludar(String nombre) {
-    System.out.println(" Hola " +nombre);
-    System.out.println("Dale duro a la programación");
-    System.out.println("No te desanime");
-    System.out.println("Lo estas haciendo genial");
+//public static void saludar(String nombre) {
+    //System.out.println(" Hola " +nombre);
+    //System.out.println("Dale duro a la programación");
+    //System.out.println("No te desanime");
+    //System.out.println("Lo estas haciendo genial");
 }
 
-}
+//}
 /* Hasta ahora lo que se ha hecho es refactorizar y también ahorrar lineas de codigo, podría haberlo escrito a mano uno por uno con distintos nombres
 pero no interesa, es más efica y si hay que modificar algo es más facil modificarlo ya que al agrupar el codigo, si hay que cambiar algo
 es más facil el poder solucionarlo si el problema afecta a todo. Es decir, en vez de escribirlo con Systemoutprintln lo que se hace
 es crear un metodo, en este caso el metodo se llama saludar, no es como las variables que primero hay que definirlas para luego poder usarlas.
 Si desde donde "llamo" al metodo saludar tiene el codigo con public static void, el nuevo metodo que yo creo debe tener lo mismo. Como yo quiero que me ponga lo mismo menos el nombre
 debe tener entonces la palabra String y luego nombre.
+
+03/11/2025: otra manera de usar las variables sería con el Scanner para ello lo que hago sería como hasta ahora que he creado nuevas funciones
+Pongo la palabra Scanner y le doy a enter porque es una variable compleja que lo que hace es me permite entrar en las librerias que hay en Java
+Y de esta manera la "llamo" luego pongo el nombre de la variable que haya creado en la otra clase, le pongo la palabra reservada new, luego otra vez Scanner
+y entre parentesis tengo que poner System.in que lo que quiere decir es que necesita un tipo de entrada que en este caso es por teclado, es decir, por consola
+
+Luego pongo las preguntas que yo quiero que el usario me respondiese, en este caso el salario, la edad y el carnet con coche, otra opción sería nombre, separar carnet y coche, estudios,etc..
+Y ahora, con las variables que puse en la clase de Calculadora donde está la funcion que he creado evaluarCandidato tengo que ir poniendo lo siguiente:
+como tengo puesto double tengo que poner double aquí también para el salario y pongo la variable salario = evaluarCandidato (mi funcion).nextDouble() y así con cada una de las preguntas.
+Se pone debajo de la pregunta que he realizado.
+Una vez hecho eso hay que poner operacionesMatematicas(funcion que yo he creado en Main).evaluarCandidato(funcion que he creado en Calculadora)(salario,edad, cochePropio).
 */
